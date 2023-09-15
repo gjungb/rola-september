3ab6f22db33c3d70f211d4a3dc7c55383f7cf97a
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2023-09-15 18:06:36 +0200

    feat: add color validator

--- src/app/color-form/color-form.component.ts
+++ src/app/color-form/color-form.component.ts
@@ -1,24 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+} from '@angular/forms';
+import { TinyColor } from '@ctrl/tinycolor';
+import { tap } from 'rxjs';
+import { Led } from '../model/led';
 
+/**
+ * Validator for color strings
+ */
+const isValidColor: ValidatorFn = ({ value }: AbstractControl) =>
+  new TinyColor(value ?? undefined).isValid
+    ? null
+    : { color: `Invalid color: ${value}` };
+
+/**
+ *
+ */
 @Component({
   selector: 'pi-color-form',
   templateUrl: './color-form.component.html',
   styleUrls: ['./color-form.component.scss'],
 })
 export class ColorFormComponent implements OnInit {
+  #dr = inject(DestroyRef);
+
   form: FormGroup = new FormGroup({
     color: new FormControl(null, {
-      validators: [Validators.required, Validators.minLength(3)],
+      validators: [isValidColor],
     }),
   });
 
+  /**
+   *
+   */
   ngOnInit(): void {
-    this.form.valueChanges.pipe();
+    this.form.statusChanges
+      .pipe(
+        tap(() => console.log(this.form.errors)),
+        takeUntilDestroyed(this.#dr)
+      )
+      .subscribe();
   }
 
-  updateColor(): void {
+  /**
+   *
+   * @param value
+   */
+  updateColor(value: Pick<Led, 'color'>): void {
     // effect
-    console.log('value');
+    console.log(value);
   }
 }
